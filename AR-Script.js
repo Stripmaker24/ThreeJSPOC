abef6e127352376a4fbbd520f62d85e7b65e11ab
Stripmaker24 <33493759+Stripmaker24@users.noreply.github.com>
2022-03-08 11:46:46 +0100

    Update

--- AR-Script.js
+++ AR-Script.js
@@ -1,24 +1,71 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 import { ARButton } from 'https://unpkg.com/three/examples/jsm/webxr/ARButton.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var container;
+var camera, scene, renderer;
+var controller;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+init();
+animate();
 
-const map = new THREE.TextureLoader().load( 'sprite.png' );
-const material = new THREE.SpriteMaterial( { map: map } );
+function init() {
 
-const sprite = new THREE.Sprite( material );
-scene.add( sprite );
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
 
-camera.position.z = 5;
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
+
+    var light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    light.position.set( 0.5, 1, 0.25 );
+    scene.add( light );
+
+    //
+
+    renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.xr.enabled = true;
+    container.appendChild( renderer.domElement );
+
+    //
+
+    document.body.appendChild( ARButton.createButton( renderer ) );
+
+    //
+
+    const map = new THREE.TextureLoader().load( 'sprite.png' );
+    const material = new THREE.SpriteMaterial( { map: map });
+    
+    const sprite = new THREE.Sprite( material );
+    scene.add( sprite );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+//
 
 function animate() {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
-};
 
-animate();
+    renderer.setAnimationLoop( render );
+
+}
+
+function render() {
+
+    renderer.render( scene, camera );
+
+}
