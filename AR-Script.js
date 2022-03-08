25e962f7e0f25ccdc7d01ebc4d45d6731bb901b1
Stripmaker24 <33493759+Stripmaker24@users.noreply.github.com>
2022-03-08 11:44:49 +0100

    Sprite test2

--- AR-Script.js
+++ AR-Script.js
@@ -1,87 +1,24 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 import { ARButton } from 'https://unpkg.com/three/examples/jsm/webxr/ARButton.js';
 
-var container;
-var camera, scene, renderer;
-var controller;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-init();
-animate();
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-function init() {
+const map = new THREE.TextureLoader().load( 'sprite.png' );
+const material = new THREE.SpriteMaterial( { map: map } );
 
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
+const sprite = new THREE.Sprite( material );
+scene.add( sprite );
 
-    scene = new THREE.Scene();
-
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
-
-    var light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-    light.position.set( 0.5, 1, 0.25 );
-    scene.add( light );
-
-    //
-
-    renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.xr.enabled = true;
-    container.appendChild( renderer.domElement );
-
-    //
-
-    document.body.appendChild( ARButton.createButton( renderer ) );
-
-    //
-
-    // var geometry = new THREE.CylinderBufferGeometry( 0, 0.05, 0.2, 32 ).rotateX( Math.PI / 2 );
-
-    // function onSelect() {
-
-    //     var material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
-    //     var mesh = new THREE.Mesh( geometry, material );
-    //     mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
-    //     mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
-    //     scene.add( mesh );
-
-    // }
-
-    // controller = renderer.xr.getController( 0 );
-    // controller.addEventListener( 'select', onSelect );
-    // scene.add( controller );
-
-    var map = new THREE.TextureLoader().load( "sprite.png" );
-    var material = new THREE.SpriteMaterial( { map: map, color: 0xffffff } );
-    var sprite = new THREE.Sprite( material );
-    sprite.scale.set(500,562,1);
-    scene.add( sprite );
-
-    //
-
-    window.addEventListener( 'resize', onWindowResize, false );
-
-}
-
-function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-//
+camera.position.z = 5;
 
 function animate() {
-
-    renderer.setAnimationLoop( render );
-
-}
-
-function render() {
-
+    requestAnimationFrame( animate );
     renderer.render( scene, camera );
+};
 
-}
+animate();
