89a7db94d324dba2292075e45e343e750fcc1758
Vincent <63718020+Vincent2162102@users.noreply.github.com>
2022-03-09 20:18:11 +0100

    added sidelines to graph, still not working based on size of the chart

---
+++ Controller/MainController.js
@@ -0,0 +1,25 @@
+import cars from "../Data/Cars.json" assert { type: "json" };
+
+export default class MainController{
+
+    graphdata = {};
+
+    readJson(){
+        let data = cars;
+        
+        for (let index = 0; index < data.length; index++) {
+            for (let j = 0; j < data[index].cars.length; j++) {
+                
+                if(!(data[index].cars[j].color in this.graphdata)) {
+
+                        this.graphdata[ data[index].cars[j].color ] = 1;
+                }else{
+                        this.graphdata[data[index].cars[j].color] += 1;
+                }
+
+            }
+        }
+        return this.graphdata;
+    }
+
+}
--- THREE-Script.js
+++ THREE-Script.js
@@ -2,11 +2,11 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
 //TEMP_DATA
-const data = [2,10,5,6,3,8,7]
 
+const data = [2,10,5,6,3,8,7]
 //SET_UP
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 //RENDERS
 const renderer = new THREE.WebGLRenderer();
@@ -15,12 +15,39 @@ document.body.appendChild( renderer.domElement );
 
 //CUBES
 const cubeGroup = new THREE.Group();
-const material = new THREE.MeshBasicMaterial({color:0xC0C0C0});
 const cubeWidth = 1;
 const cubeDepth = 1;
 let position = -4;
 
-data.forEach(makeCube)
+
+generateBarChart()
+
+function generateBarChart(){
+
+	let counter = 0;
+	data.forEach((e) => {
+		makeCube(e);
+		counter++;
+	})
+
+	//SIDELINES left
+	const terial = new THREE.MeshBasicMaterial();
+	var box = new THREE.BoxGeometry(0, (counter * 1.5), 1);
+	var sidetwo = new THREE.Mesh(box, terial);
+	sidetwo.position.set(-5.5, 5.2, 0);
+	cubeGroup.add(sidetwo);
+
+	//SIDELINES right
+	const mat = new THREE.MeshBasicMaterial();
+	var bla = new THREE.BoxGeometry((counter * 2), 0.1, 1);
+	var sideone = new THREE.Mesh(bla, mat);
+	sideone.position.set(1.5, 0, 0);
+	cubeGroup.add(sideone);
+
+	
+	
+
+}
 
 scene.add(cubeGroup);
 camera.position.set(0,5,10);
@@ -33,9 +60,12 @@ function animate() {
 animate();
 
 function makeCube(item, index){
+	const material = new THREE.MeshBasicMaterial();
+	material.color.setHex("0x" + Math.floor(Math.random()*16777215).toString(16))
+
     var geometry = new THREE.BoxGeometry(cubeWidth, item, cubeDepth);
     var cube = new THREE.Mesh(geometry, material);
-    cube.position.set(position, 0+(item/2), 0);
+    cube.position.set(position, 0.1+(item/2), 0);
     console.log(cube.position)
     position += 2;
     cubeGroup.add(cube);
